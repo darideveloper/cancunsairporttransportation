623769d2a18a8f7b891d9e29074717d4c88a3750
daridev <darideveloper@gmail.com>
2026-01-05 14:08:14 -0600

    feat: setup prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import { defineConfig } from 'astro/config'
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
 
-import react from '@astrojs/react';
+import react from '@astrojs/react'
 
-import showTailwindcssBreakpoint from 'astro-show-tailwindcss-breakpoint';
+import showTailwindcssBreakpoint from 'astro-show-tailwindcss-breakpoint'
 
 // https://astro.build/config
 export default defineConfig({
@@ -23,4 +23,4 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-});
+})
