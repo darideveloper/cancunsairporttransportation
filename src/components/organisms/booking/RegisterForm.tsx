435a290505d5b4836e255e92e41ef51e93d986ae
daridev <darideveloper@gmail.com>
2026-03-09 17:50:24 -0600

    style (RegisterFormm):: responsive layout

--- src/components/organisms/booking/RegisterForm.tsx
+++ src/components/organisms/booking/RegisterForm.tsx
@@ -20,13 +20,13 @@ export default function RegisterForm({ lang }: RegisterFormProps) {
   return (
     <>
       <SelectedVehicleCard lang={lang} />
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
-        <div className="left col-span-2 flex flex-col gap-8">
+      <div className="flex flex-col gap-4 lg:flex-row">
+        <div className="left flex w-full flex-col gap-8">
           <ArrivalInformation lang={lang} />
           <ReturnInformation lang={lang} />
           <PassengerInformation lang={lang} />
         </div>
-        <div className="right col-span-1 flex flex-col gap-4">
+        <div className="right flex w-full flex-col gap-4 lg:max-w-sm">
           <BookingSummary
             lang={lang}
             selectedVehicle={selectedVehicle}
